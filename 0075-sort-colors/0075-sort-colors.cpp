2a22bb961e99ac22310dbd4e86e5f0a3db1ca403
naresh kumar <103481329+naresh2002kum@users.noreply.github.com>
2023-07-06 00:37:29 +0530

    Time: 3 ms (47.11%), Space: 8.2 MB (82.67%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void insertionSort(std::vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+public:
+    void sortColors(vector<int>& nums) {
+        insertionSort(nums);
+    }
+};
