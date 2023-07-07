a2873d2893215411bad92ceef7add996f22965a0
naresh kumar <103481329+naresh2002kum@users.noreply.github.com>
2023-07-07 19:10:06 +0530

    Time: 61 ms (90.18%), Space: 13.1 MB (72.74%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,47 @@
+//optimal approach-copy
+//t.c-o(n^3)
+//s.c-o(number of quarduple)==o(1)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            //to avoid duplicate while moving i
+            if(i>0 &&nums[i]==nums[i-1]) continue;
+            
+            for(int j=i+1;j<n;j++){
+                    //to avoid duplicate while moving j
+                    if(j>i+1 &&nums[j]==nums[j-1]) continue;
+                
+                int k=j+1,l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                     sum+=nums[j];
+                     sum+=nums[k];
+                    sum+=nums[l];
+                    
+                    if(sum==target){
+                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++,l--;
+                        
+                        
+                        //skip duplicate while moving k and l
+                        while(k<l &&nums[k]==nums[k-1]) 
+                            k++;
+                        while(k<l &&nums[l]==nums[l+1])
+                            l--;
+                    }
+                    else if(sum<target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+        return ans;
+    }
+};
