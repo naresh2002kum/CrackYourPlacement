9eae5d90b8ffac1ec8a3bdb36a06f5db4b067077
naresh kumar <103481329+naresh2002kum@users.noreply.github.com>
2023-07-08 16:57:47 +0530

    Time: 15 ms (93.41%), Space: 19.5 MB (53.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,31 @@
-//beter approach, t.c-o(nlog)+o(n) ,s.c-
+//MOORE voting algorithm
+//optimal approach, t.c-o(n) ,s.c-1
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>mp;
+        int cnt=0;
+        int element;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+            if(cnt==0){
+                cnt=1;
+                element=nums[i];
+            }else if(nums[i]==element){
+                cnt++;
+            }else{
+                cnt--;
+            }
         }
-        for(auto it:mp){
-            if(it.second>n/2)
-                return it.first;
+        
+        int cnt1=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==element){
+                cnt1++;
+            }
+            if(cnt1>n/2)
+                return element;
         }
         return -1;
+        
     }
 };
