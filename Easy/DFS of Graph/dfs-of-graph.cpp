50de9f45f86c45054bfaaede8e799d808f31d29f
naresh kumar <103481329+naresh2002kum@users.noreply.github.com>
2023-09-19 19:30:30 +0530

    Updated Solution - GfG to GitHub

--- Easy/DFS of Graph/dfs-of-graph.cpp
+++ Easy/DFS of Graph/dfs-of-graph.cpp
@@ -4,32 +4,62 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-public:
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
-        vector<int> dfsTraversal;
-        vector<bool> visited(V, false);
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, adj, dfsTraversal);
+    private:
+    void dfs(int node, vector<int> adj[],vector<int>&ans,vector<bool>&vis){
+        
+        vis[node]=true;
+        ans.push_back(node);
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,ans,vis);
             }
         }
-
-        return dfsTraversal;
     }
-
-private:
-    void dfs(int node, vector<bool>& visited, vector<int> adj[], vector<int>& dfsTraversal) {
-        visited[node] = true;
-        dfsTraversal.push_back(node);
-
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, visited, adj, dfsTraversal);
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<bool>vis(V,false);
+        vector<int>ans;
+        
+        //for disconnected component
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(0,adj,ans,vis);
             }
         }
+       
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+//         vector<int> dfsTraversal;
+//         vector<bool> visited(V, false);
+
+//         for (int i = 0; i < V; i++) {
+//             if (!visited[i]) {
+//                 dfs(0, visited, adj, dfsTraversal);
+//             }
+//         }
+
+//         return dfsTraversal;
+//     }
+
+// private:
+//     void dfs(int node, vector<bool>& visited, vector<int> adj[], vector<int>& dfsTraversal) {
+//         visited[node] = true;
+//         dfsTraversal.push_back(node);
+
+//         for (int neighbor : adj[node]) {
+//             if (!visited[neighbor]) {
+//                 dfs(neighbor, visited, adj, dfsTraversal);
+//             }
+//         }
+//     }
+// };
   
 
 //{ Driver Code Starts.
